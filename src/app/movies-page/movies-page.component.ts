c0bd81aa5c7ae33cfdb8f35f89987b46caa25e46
Alejandra Català <43583723+acatalas@users.noreply.github.com>
2024-12-10 22:22:07 +0100

    Added simple search to MoviesPage

--- src/app/movies-page/movies-page.component.ts
+++ src/app/movies-page/movies-page.component.ts
@@ -1,15 +1,26 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { MoviesService } from '../services/movies.service';
-import { MovieFormComponent } from '../movie-form/movie-form.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'movies-page',
-  imports: [MovieCardComponent, MovieFormComponent],
+  imports: [MovieCardComponent, FormsModule],
   templateUrl: './movies-page.component.html',
   styleUrl: './movies-page.component.css'
 })
 export class MoviesPageComponent {
   moviesService = inject(MoviesService)
   movies = this.moviesService.getMovies();
+
+  search = signal('');
+
+  filteredMovies = computed(() => {
+    const searchText = this.search().toLowerCase();
+    return this.movies.filter(
+      (e) =>
+        e.title.toLowerCase().includes(searchText)
+    );
+  });
+  
 }
